a7ba0b5f9fac5a63d51ee5a24ae1b4d16dac6207
sebastian2929 <62757013+sebastian2929@users.noreply.github.com>
2023-02-02 19:01:11 -0500

    Add files via upload

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,23 @@
+package edu.eci.cvds.patterns.shapes;
+
+public class ShapeFactory() {
+    public static Shape create(RegularShapeType type) {
+        
+    String shape = null;
+
+        switch(shape){
+            case"Triangle":
+
+            case"Quadrilatral":
+
+            case"Pentagon":
+
+            case"Hexagon":
+
+            default:
+                return
+        }
+    }
+}
+
+
